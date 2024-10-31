37ee97753d1b5e368341d77d7d470862253dbd15
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-10-31 08:58:38 +0100

    Added style changes to homepage

--- app/_components/HomeAbout.tsx
+++ app/_components/HomeAbout.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function HomeAbout() {
   return (
-    <div className="xl:px-20 px-5 md:px-10 min-h-[400px]">
+    <div className="xl:px-20 px-5 md:px-10 min-h-[400px] bg-foreground">
         <div className='flex justify-center'>
-            <h2 className='py-8'> About YLF Abuja</h2>
+            <h2 className='py-8 text-background'> About YLF Abuja</h2>
         </div>
         <div className='xl:flex'>
             <div className='xl:w-[30%]'>
@@ -19,12 +19,12 @@ function HomeAbout() {
                 </ol>
             </div>
             <div className='xl:w-[70%]'>
-                <p className='xl:pt-0 pt-5'>
+                <p className='xl:pt-0 pt-5 text-background'>
                     The Young Lawyers Forum of Nigerian Bar Association, Abuja Branch (Unity Bar) is as a gateway for young lawyers to enter and thrive in the legal community, promoting their welfare and active participation. Our mission is to empower young lawyers in Abuja by fostering a supportive community, promoting professional development, and advocating for their interests. <br></br> <br></br> The Forum was established by the then Chairman of the Branch Abdul Ibrahim SAN with Mr Afam Okeke the immediate past Chairman of the Branch as the Pioneer Chairman of the Forum in 2006.
                 </p>
                 <div>
-                    <a className='button my-20' href='/about'>
-                      <div className='flex items-center btn hover:translate-x-2 gap-3.5'>
+                    <a className='button my-20 bg-background' href='/about'>
+                      <div className='flex items-center bg-background btn hover:translate-x-2 gap-3.5'>
                       <span>learn more</span>
                       <svg viewBox="0 0 32 32" width="30" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M22 9a1 1 0 0 0 0 1.42l4.6 4.6H3.06a1 1 0 1 0 0 2h23.52L22 21.59A1 1 0 0 0 22 23a1 1 0 0 0 1.41 0l6.36-6.36a.88.88 0 0 0 0-1.27L23.42 9A1 1 0 0 0 22 9Z" data-name="Layer 2" fill="#c9a66d" ></path></svg>
                       </div>
--- app/_components/HomeChairman.tsx
+++ app/_components/HomeChairman.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 function HomeChairman() {
   return (
     <div className='relative'>
-        <div className="chairman bg-cover bg-center bg-fixed xl:px-20 px-5 md:px-10 min-h-[600px] py-20 xl:flex-row flex flex-col items-center">
+        <div className="chairman bg-cover bg-center bg-fixed xl:px-20 px-5 md:px-10 min-h-[600px] py-20 xl:flex-row flex flex-col items-center bg-foreground">
             <div className='xl:w-1/2 z-20'>
-                <h2 className='z-20'>Meet the Chairman</h2>
-                <p className='text-3xl pt-8'>Najib A. Waziri</p>
+                <h2 className='z-20 text-background'>Meet the Chairman</h2>
+                <p className='text-3xl pt-8 text-background'>Najib A. Waziri</p>
                 <p className='small-gold'> Chairman, NBA YLF Abuja</p>
                 <div>
-                    <a className='button my-10' href='https://www.linkedin.com/in/najib-waziri-3335591ab' target='_blank'>
+                    <a className='button my-10 bg-background' href='https://www.linkedin.com/in/najib-waziri-3335591ab' target='_blank'>
                         <div className='flex items-center btn hover:translate-x-2 gap-3.5'>
                         <span>view profile</span>
                         <svg viewBox="0 0 32 32" width="30" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M22 9a1 1 0 0 0 0 1.42l4.6 4.6H3.06a1 1 0 1 0 0 2h23.52L22 21.59A1 1 0 0 0 22 23a1 1 0 0 0 1.41 0l6.36-6.36a.88.88 0 0 0 0-1.27L23.42 9A1 1 0 0 0 22 9Z" data-name="Layer 2" fill="#c9a66d" ></path></svg>
