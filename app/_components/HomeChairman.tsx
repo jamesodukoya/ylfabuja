b2b517606d38a5f984fcf4d3be615c21d030bbd3
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-11-04 22:22:37 +0100

    Changed chairman profile link

--- app/_components/HomeChairman.tsx
+++ app/_components/HomeChairman.tsx
@@ -9,7 +9,7 @@ function HomeChairman() {
                 <p className='text-3xl pt-8 text-background'>Najib A. Waziri</p>
                 <p className='small-gold'> Chairman, NBA YLF Abuja</p>
                 <div>
-                    <a className='button my-10 bg-background' href='https://www.linkedin.com/in/najib-waziri-3335591ab' target='_blank'>
+                    <a className='button my-10 bg-background' href='https://www.najibwaziri.com' target='_blank'>
                         <div className='flex items-center btn hover:translate-x-2 gap-3.5'>
                         <span>view profile</span>
                         <svg viewBox="0 0 32 32" width="30" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M22 9a1 1 0 0 0 0 1.42l4.6 4.6H3.06a1 1 0 1 0 0 2h23.52L22 21.59A1 1 0 0 0 22 23a1 1 0 0 0 1.41 0l6.36-6.36a.88.88 0 0 0 0-1.27L23.42 9A1 1 0 0 0 22 9Z" data-name="Layer 2" fill="#c9a66d" ></path></svg>
