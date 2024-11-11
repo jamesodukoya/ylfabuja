0bc5fbcf92564f21304c4a2a90a5d2c9dd49d0e7
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-11-11 19:08:36 +0100

    Added gallery

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -30,6 +30,12 @@ export const Executives = [
         sm_picture: '/sm_JenniferBen-AgandeSocialSecretary.webp'
     },
     {
+        name: 'Ahmad Ishaq',
+        office: 'Publicity Secretary',
+        picture: '/AhmadIshaq.webp',
+        sm_picture: '/sm_AhmadIshaq.webp'
+    },
+    {
         name: 'Simiat Suleiman',
         office: 'Treasurer',
         picture: '/SimiatSulemanTreasurer.webp',
@@ -59,4 +65,103 @@ export const Executives = [
         picture: '/NadiSochimaLyndaMember.webp',
         sm_picture: '/sm_NadiSochimaLyndaMember.webp'
     }
+]
+
+export const GalleryItems = [
+    {
+        image: '/ChairmanELawyersSummit.jpeg',
+        thumbnail: '/thumbChairmanELawyersSummit.jpeg',
+        caption: 'The Chairman at E Lawyers Summit in Abuja in October 2024'
+    },
+    {
+        image: '/ChairmanAfamOsigwe.jpeg',
+        thumbnail: '/thumbChairmanAfamOsigwe.jpeg',
+        caption: 'Chairman with President of the Nigerian Bar Association, MAZI Afam Osigwe'
+    },
+    {
+        image: '/ChairmanWestAfricanLawStudentsAssoc.jpeg',
+        thumbnail: '/thumbChairmanWestAfricanLawStudentsAssoc.jpeg',
+        caption: 'Chairman at the West African Law Students Association Annual Summit held at the National Secretariat of the NBA, Abuja'
+    },
+    {
+        image: '/ChairmanObegolu.jpeg',
+        thumbnail: '/thumbChairmanObegolu.jpeg',
+        caption: 'Chairman together with Chief Emeka J P Obegolu SAN, Paul Harris Ogbole SAN and the President of the of West African Law Students Association (WALSA) at the WALSA Annual Summit held at the National Secretariat of the NBA, Abuja'
+    },
+    {
+        image: '/ChairmanMuhammedAdam.jpeg',
+        thumbnail: '/thumbChairmanMuhammedAdam.jpeg',
+        caption: 'Chairman with Muhammed Adam Esq, Past Chairman of NBA Women Forum Abuja'
+    },
+    {
+        image: '/ChairmanSeniorMembersBar.jpeg',
+        thumbnail: '/thumbChairmanSeniorMembersBar.jpeg',
+        caption: 'The Chairman and other Senior Members of the Bar'
+    },
+    {
+        image: '/ChairmanWALSASummit.jpeg',
+        thumbnail: '/thumbChairmanWALSASummit.jpeg',
+        caption: 'The Chairman delivering a goodwill message at the WALSA annual summit'
+    },
+    {
+        image: '/ChairmanPubSecDigitspot.jpeg',
+        thumbnail: '/thumbChairmanPubSecDigitspot.jpeg',
+        caption: 'The Chairman and Publicity Secretary at the Digitspot Program in Abuja'
+    },
+    {
+        image: '/ChairmanBranchDigitspot.jpeg',
+        thumbnail: '/thumbChairmanBranchDigitspot.jpeg',
+        caption: 'The Chairman of the Branch delivering his remarks at the Digitspot program'
+    },
+    {
+        image: '/DigitspotProgram.jpeg',
+        thumbnail: '/thumbDigitspotProgram.jpeg',
+        caption: 'Digitspot Program'
+    },
+    {
+        image: '/Digitspot.jpeg',
+        thumbnail: '/thumbDigitspot.jpeg',
+        caption: 'Digispot program'
+    },
+    {
+        image: '/ChairmanPubSecOthers.jpeg',
+        thumbnail: '/thumbChairmanPubSecOthers.jpeg',
+        caption: 'The Chairman, Publicity Secretary and other Young Lawyers at the Digitspot Program'
+    },
+    {
+        image: '/ChairmanNBAGalaxyBackbone.jpeg',
+        thumbnail: '/thumbChairmanNBAGalaxyBackbone.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairmanNBAGalaxy.jpeg',
+        thumbnail: '/thumbChairmanNBAGalaxy.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairmanNBADelegationGalaxy.jpeg',
+        thumbnail: '/thumbChairmanNBADelegationGalaxy.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairmanGalaxy.jpeg',
+        thumbnail: '/thumbChairmanGalaxy.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairmanAtGalaxy.jpeg',
+        thumbnail: '/thumbChairmanAtGalaxy.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairGalaxy.jpeg',
+        thumbnail: '/thumbChairGalaxy.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+    {
+        image: '/ChairGalaxyBackbone.jpeg',
+        thumbnail: '/thumbChairGalaxyBackbone.jpeg',
+        caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
+    },
+
 ]
---
+++ app/gallery/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+import React, { useEffect } from 'react'
+import Header from '../_components/Header'
+import fjGallery from 'flickr-justified-gallery';
+import LightGallery from 'lightgallery/react';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgVideo from 'lightgallery/plugins/video';
+import 'lightgallery/css/lightgallery.css';
+import { GalleryItems } from '../(data)/Templates';
+
+function Gallery() {
+
+    useEffect(() => {
+    fjGallery(document.querySelectorAll('.gallery'), {
+      itemSelector: '.gallery__item',
+      rowHeight: 280,
+      lastRow: 'start',
+      gutter: 5,
+      rowHeightTolerance: 0.1,
+      calculateItemsHeight: false,
+    });
+  }, []);
+
+  return (
+    <div className=''>
+        <div className="relative z-10">
+            <section className="soon bg-cover bg-center bg-fixed">
+                <Header />
+                <div className="xl:px-20 px-5 md:px-10 pt-28 min-h-[400px] flex items-center justify-center">
+                    <div className='py-32 relative z-10'>
+                        <h1 className='text-center'>Gallery</h1>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div className='xl:px-20 px-5 md:px-10'>
+            <LightGallery
+        plugins={[lgZoom, lgVideo]}
+        mode="lg-fade"
+        pager={true}
+        thumbnail={true}
+        galleryId={'nature'}
+        autoplayFirstVideo={false}
+        elementClassNames={'gallery'}
+        mobileSettings={{
+          controls: false,
+          showCloseIcon: false,
+          download: false,
+          rotate: false,
+        }}
+      >
+        {GalleryItems.map((photo, index)=>(
+        <a
+          data-lg-size="1600-2400"
+        //   data-pinterest-text="Pin it2"
+        //   data-tweet-text="lightGallery slide  2"
+          className="gallery__item"
+          data-src={photo.image}
+          data-sub-html={`<h4>${photo.caption}</h4>`}
+        >
+          <img
+            className="img-responsive"
+            src={photo.thumbnail}
+          />
+        </a>
+        ))}
+      </LightGallery>
+        </div>
+    </div>
+  )
+}
+
+export default Gallery
