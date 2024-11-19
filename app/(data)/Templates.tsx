f7cf57a8bf1d2750799a39c4a3f51c640a8167be
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-11-19 02:13:41 +0100

    Populated law firms page & added search

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -163,5 +163,462 @@ export const GalleryItems = [
         thumbnail: '/thumbChairGalaxyBackbone.jpeg',
         caption: 'The Chairman as part of the NBA delegation to Galaxybackbone'
     },
+]
 
+export const LawFirmsInAbuja = [
+  {
+    name: "SILVERWRIT ATTORNEYS",
+    address: "14 Alexandria Cres, Wuse 2, Abuja",
+    number: 9167935575,
+    website: "",
+    geocode: [9.083782,7.465751]
+  },
+  {
+    name: "Kayode Ajulo & Co. Castle of Law",
+    address: "21 Amazon St, Maitama, Abuja",
+    number: 7419001649,
+    website: "https://castleoflaw.com",
+    geocode: [9.09883,7.484866]
+  },
+  {
+    name: "A.O AYENI & ASSOCIATES",
+    address: "3FM4+28V Trinity House, Mabushi, Ndawuse 900108, Federal Capital Territory",
+    number: 7039025363,
+    website: "http://aoayenilawfirm.net",
+    geocode: [9.082625,7.45581]
+  },
+  {
+    name: "P. D. PIUS & ASSOCIATES",
+    address: "Area 7, Block 2 Flat 4, off Mbaise Cl, Yola St, Garki, Abuja",
+    number: 8038944639,
+    website: "http://www.pdpiusandassociates.com",
+    geocode: [9.036335,7.486974]
+  },
+  {
+    name: "OSUYA & OSUYA LAW FIRM",
+    address: "3 Toamasina Street, off Adetokunbo Ademola Cres, Wuse 2, Abuja",
+    number: 2092910888,
+    website: "https://www.osuyalawfirm.com.ng",
+    geocode: [9.069923,7.487938]
+  },
+  {
+    name: "S.I.O OKPUNO LEGAL SERVICES",
+    address: "12 Nairobi St, Wuse 2, Abuja",
+    number: 8063746401,
+    website: "https://www.siookpunolegalservices.com",
+    geocode: [9.076749,7.465147]
+  },
+  {
+    name: "Greengold Attorneys",
+    address: "Suite 30B, Copper House, Wuse Zone 5, FCT, Wuse, Abuja",
+    number: 7019282250,
+    website: "https://greengoldattorneys.com",
+    geocode: [9.063285,7.456871]
+  },
+  {
+    name: "D. D. Dodo & Co.",
+    address: "10 Atbara St, off Cairo Crescent, Wuse 2, Abuja",
+    number: 8146657555,
+    website: "http://dddodo.com",
+    geocode: [9.077206,7.479224]
+  },
+  {
+    name: "LawHaven Solicitors & Advocates",
+    address: "Zone 1, DBM Plaza, Wuse, Abuja",
+    number: 8077331680,
+    website: "http://www.lawhavensolicitors.com",
+    geocode: [9.080362,7.464748]
+  },
+  {
+    name: "Hammed Lasisi & Associates",
+    address: "51 Moses Majekodunmi Street, Utako, Abuja 8 Unity Close, Arab Rd, Kubwa, Abuja",
+    number: 8165461795,
+    website: "",
+    geocode: [9.164023,7.316569]
+  },
+  {
+    name: "Olamide Oyetayo & Co",
+    address: "Suite 041, Trinity House, behind Federal Ministry Of Works, Mabushi, Abuja",
+    number: 8068154338,
+    website: "https://olamideoyetayolegal.com",
+    geocode: [9.082625,7.45581]
+  },
+  {
+    name: "Vantage Attorneys LP",
+    address: "30 Libreville Street, off Aminu Kano Cres, Wuse 2, Abuja",
+    number: 9160672000,
+    website: "https://vantageattorneyslp.ng",
+    geocode: [9.07483,7.461823]
+  },
+  {
+    name: "Solola & Akpana",
+    address: "12 River Patoka Close, Off Nile St, Maitama, Abuja",
+    number: 92915460,
+    website: "http://sololaakpana.ng",
+    geocode: [9.094339,7.482484]
+  },
+  {
+    name: "STRATAGEM LEGAL PRACTITIONERS",
+    address: "Area 11, 6 Umuahia Cl, off Emeka Anyaoku Street, Garki, Abuja",
+    number: "",
+    website: "http://stratagemlp.com",
+    geocode: [9.043484,7.497312]
+  },
+  {
+    name: "Omaplex Law Firm",
+    address: "Plot 994, Edwin Ume Ezeoke Street, Ameh Ebute St, off Ahmeh, Wuye, Abuja",
+    number: 8184854433,
+    website: "https://omaplex.com.ng",
+    geocode: [9.04982,7.433785]
+  },
+  {
+    name: "LAW CAPITOL",
+    address: "Plot 1132, (by Aso Radio) Katampe, Abuja",
+    number: 8128357041,
+    website: "https://www.thelawcapitol.com",
+    geocode: [7.474255,9.114161]
+  },
+  {
+    name: "Mahmud and Co",
+    address: "No. 9 Bozoum, Wuse 2, Abuja 904101, Federal Capital Territory",
+    number: "",
+    website: "",
+    geocode: [9.072817,7.477407]
+  },
+  {
+    name: "Alegeh & Co.",
+    address: "3F8C+7HV, Loma Mansa St, Wuse, Abuja",
+    number: "",
+    website: "",
+    geocode: [9.06633,7.472364]
+  },
+  {
+    name: "J-K Gadzama LLP",
+    address: "J-K Gadzama Court, Plot 1805 Damaturu Crescent by Kabo Way, Off Ahmadu Bello Way, Garki 2, Abuja",
+    number: 96233600,
+    website: "http://www.j-kgadzamallp.com",
+    geocode: [9.028168,7.494915]
+  },
+  {
+    name: "Kanu G. Agabi & Associates",
+    address: "Trinity House, Mabushi, Abuja",
+    number: 8075752984,
+    website: "",
+    geocode: [9.082625,7.45581]
+  },
+  {
+    name: "Paramount Chambers And Notary Public",
+    address: "Suite D23, Danziyal Shopping Plaza, Nal Blvd, Central Business District, Abuja",
+    number: 9037902114,
+    website: "https://paramountchambersnotaries.weebly.com",
+    geocode: [9.052869,7.477067]
+  },
+  {
+    name: "Madyan Legal Consult",
+    address: "Area 11, 4 Mubi Cl, off Emeka Anyaoku Street, Garki, Abuja",
+    number: 8067681669,
+    website: "",
+    geocode: [9.040175,7.494093]
+  },
+  {
+    name: "BIMAK Associates",
+    address: "Flat 2, Block D, 1 Patricia Etteh Cl, Wuye, Abuja",
+    number: "",
+    website: "",
+    geocode: [9.045047,7.443841]
+  },
+  {
+    name: "A.U.MUSTAPHA & CO",
+    address: "32 Mediterranean St, District, Abuja",
+    number: 8142199584,
+    website: "",
+    geocode: [9.086899,7.488638]
+  },
+  {
+    name: "Omimi-Afua Legal Hub",
+    address: "House 4, Road E, Jabi, Abuja",
+    number: 8099996918,
+    website: "",
+    geocode: [9.042214,7.501819]
+  },
+  {
+    name: "REN ATTORNEYS",
+    address: "AREA 10, 14 Makurdi St, Central Business Dis, Abuja",
+    number: 8096152000,
+    website: "http://www.renattorneys.com",
+    geocode: [9.040767,7.482753]
+  },
+  {
+    name: "Aluko & Oyebode",
+    address: "Afri Investment House, 50 Aguiyi Ironsi St, Maitama, Abuja",
+    number: 92921083,
+    website: "http://www.aluko-oyebode.com",
+    geocode: [9.084503,7.483597]
+  },
+  {
+    name: "Dikko & Mahmoud",
+    address: "10 Seguela street, Wuse, Abuja",
+    number: "",
+    website: "http://dikkoandmahmoud.com",
+    geocode: [9.06696,7.492937]
+  },
+  {
+    name: "Banwo & Ighodalo",
+    address: "House, Plot 2669, Afri-Investment, Ground Floor, 50 Aguiyi Ironsi St, Wuse, Abuja",
+    number: 8064702209,
+    website: "http://www.banwo-ighodalo.com/",
+    geocode: [9.084495,7.483581]
+  },
+  {
+    name: "Babalakin & Co",
+    address: "Benue, Maitama, Abuja 904101, Federal Capital Territory",
+    number: 92780930,
+    website: "http://www.babalakinandco.com",
+    geocode: [9.098598,7.499483]
+  },
+  {
+    name: "Chief Afe Babalola & Co.",
+    address: "EMMANUEL HOUSE, 24, Madeira Street, Imani Estate, Maitama, Abuja.",
+    number: 8023514173,
+    website: "http://afebabalola.com",
+    geocode: [9.087644,7.491388]
+  },
+  {
+    name: "Resolution Law Firm",
+    address: "Suite 83, 3rd Floor, De Avalon Plaza, Plot 484 Obafemi Awolowo Way, Mabushi, Abuja",
+    number: 9137000474,
+    website: "www.resolutionlawng.com",
+    geocode: [9.06873328029947, 7.442043995211531]
+  },
+  {
+    name: "Liman, Liman & Co.",
+    address: "No Oba Akenzua St, Three Arms Zone, Abuja",
+    number: 8037871704,
+    website: "",
+    geocode: [9.05295137568026, 7.529016410555645]
+  },
+  {
+    name: "Obinna Ajoku SAN & Co (Legal Practitioner Firm)",
+    address: "Area 3, 4 Umudioka Close, AMAC, Abuja",
+    number: 8033203976,
+    website: "http://www.obinnaajokuandco.com",
+    geocode: [9.031043607806087, 7.484910225899652]
+  },
+  {
+    name: "Ani Martins & Associates",
+    address: "Mabushi, Abuja",
+    number: 8025422901,
+    website: "http://www.associatesng.com",
+    geocode: [9.082661620541518, 7.455843079867424]
+  },
+  {
+    name: "Dele Oye & Associates",
+    address: "Plot 575 by Livestock House, Off Obafemi Awolowo Way, Olu Awotesu St, Abuja",
+    number: 8023211760,
+    website: "http://deleoye.ng",
+    geocode: [9.06550678230199, 7.430270175637537]
+  },
+  {
+    name: "Law Corridor",
+    address: "Plot 638 Marberries St, Katampe, Abuja",
+    number: 7069358081,
+    website: "https://lawcorridor.org",
+    geocode: [9.113415999781688, 7.46903530870378]
+  },
+  {
+    name: "LEHI ATTORNEYS",
+    address: "Anon plaza, Gudu, Abuja",
+    number: 8134699398,
+    website: "https://lehiattorneys.com",
+    geocode: [9.001545628440013, 7.472480824047008]
+  },
+  {
+    name: "Banwo& Ighodalo",
+    address: "House, Plot 2669, Afri-Investment, Ground Floor, 50 Aguiyi Ironsi St, Wuse, Abuja",
+    number: 8064702209,
+    website: "http://www.banwo-ighodalo.com",
+    geocode: [9.084704658622554, 7.4836290542809785]
+  },
+  {
+    name: "THE FIRMA ADVISORY",
+    address: "1st floor Gwandal Centre, Fria Close off Fomella Crescent, off Adetokunbo Ademola Cres, Wuse 2, Abuja",
+    number: 8105847051,
+    website: "http://www.thefirmaadvisory.com",
+    geocode: [9.076193070178347, 7.48887156637555]
+  },
+  {
+    name: "Seun Olokeogun & Associates",
+    address: "6, The Penthouse Suite, Coscharis Plaza, Beside Gtb Plc, Faskari Crescent, Area 3, Garki, Abuja",
+    number: 8052358093,
+    website: "http://seunolokeogun.com.ng",
+    geocode: [9.03038048336752, 7.476775181719394]
+  },
+  {
+    name: "Starlion Legal",
+    address: "Suite C04, Gostu Plaza, 1st Avenue, Gwarinpa, Abuja",
+    number: 7057663782,
+    website: "https://starlionlegal.com",
+    geocode: [9.090134372899849, 7.408622166375642]
+  },
+  {
+    name: "Falana & Falana",
+    address: "22 Mediterranean St, Maitama, Abuja",
+    number: 8130522502,
+    website: "https://falanafalana.com.ng",
+    geocode: [9.086375186707553, 7.489011924047755]
+  },
+  {
+    name: "Ezenwafor & Co.",
+    address: "David Ejoor, Gudu, Abuja",
+    number: 7037434933,
+    website: "http://ezenwaforandco.com",
+    geocode: [9.00784482144851, 7.47027598171914]
+  },
+  {
+    name: "Alan Attorneys",
+    address: "16 Makurdi St, Central Business Dis, Abuja",
+    number: 7065455869,
+    website: "http://www.alanattorneys.com.ng",
+    geocode: [9.04120187953862, 7.482587051030978]
+  },
+  {
+    name: "El-Rufai Attorneys at Law",
+    address: "6 Jerry Gana Cl, Utako, Abuja",
+    number: 8092277776,
+    website: "https://elrufaiattorneys.com",
+    geocode: [9.075647676005635, 7.448916679867356]
+  },
+  {
+    name: "Y. C. MAIKYAU & CO.",
+    address: "Zone 1, 37A Nouakchott St, Wuse, Abuja",
+    number: 9133010000,
+    website: "https://maikyaulaw.com",
+    geocode: [9.051941262787059, 7.462614123589793]
+  },
+  {
+    name: "Zadu Attorneys and Solicitors",
+    address: "House C1, Coral Residence, by Cozgrove Estate, Plot 954 Umar Shuaibu Ave, Wuye, Abuja",
+    number: 9169019837,
+    website: "http://www.zaduattorneys.com",
+    geocode: [9.058612106460574, 7.452037966375364]
+  },
+  {
+    name: "J. S. Okutepa SAN and Co.",
+    address: "Eleojo Court Plot 2202 Apo, Zone E, Abuja",
+    number: "",
+    website: "",
+    geocode: [8.971174362189684, 7.501765078014042]
+  },
+  {
+    name: "Lex Fortis Attorneys & Solicitors",
+    address: "29 Patrick. O. Bokkor Cres, Jabi, Abuja",
+    number: 8061339650,
+    website: "http://lexfortisattorneys.com",
+    geocode: [9.069362806347307, 7.4288373375394325]
+  },
+  {
+    name: "AYD Legal",
+    address: "2A Osara Cl, Maitama, Abuja",
+    number: 9057181777,
+    website: "",
+    geocode: [9.09774696962736, 7.495081096609907]
+  },
+  {
+    name: "Praxis and Gnosis",
+    address: "Vicbalkon Towers, 14 Vicbalkon St, Utako, Abuja",
+    number: 9139148855,
+    website: "http://www.praxisgnosis.com",
+    geocode: [9.062776283996135, 7.433524095211505]
+  },
+  {
+    name: "Shariff Moulaya Chambers & Associates",
+    address: "Suite C3, Legion House, 9 Kaura- Namoda Close, Area 3, Garki, Abuja",
+    number: 8036891478,
+    website: "",
+    geocode: [9.029099398159287, 7.476389025440335]
+  },
+  {
+    name: "R A Mamza LLP",
+    address: "Area 11, 11 Ado-Ekiti Cl, Garki, Abuja",
+    number: 8182648610,
+    website: "http://www.ramamzallp.com",
+    geocode: [9.042460519059404, 7.497105022194944]
+  },
+  {
+    name: "Ace Solicitors",
+    address: "5 Gwamna Awam Street, 118 Rd, Gwarinpa Estate, Abuja",
+    number: 7035418868,
+    website: "http://www.acesolicitors.com",
+    geocode: [9.092870371278227, 7.411163766375702]
+  },
+  {
+    name: "Alu & Associates (A&A)",
+    address: "Zone 5, Anbeez Plaza, Suite 3004, Plot 16 Ndola Cres, Wuse, Abuja",
+    number: 8033549587,
+    website: "http://www.aluassociates.com",
+    geocode: [9.059745475870132, 7.455302637539348]
+  },
+  {
+    name: "Straddle Partners LP Abuja",
+    address: "7 Khartoum St, Wuse, Abuja",
+    number: 9064182062,
+    website: "",
+    geocode: [9.064254583078975, 7.467306137539367]
+  },
+  {
+    name: "Matrix Solicitors",
+    address: "Church Gate Tower, Plot 270 Muhammed Buhari Way, 3rd Floor, Grand Square, Abuja",
+    number: 8021421347,
+    website: "",
+    geocode: [9.053226513976675, 7.479384724047483]
+  },
+  {
+    name: "M.J Numa & Partners LLP",
+    address: "Flat 1, Tripple A Villas, 495 Adegboyega Atanda Street, Mabushi, Abuja",
+    number: 7033740393,
+    website: "http://www.mjnuma.com",
+    geocode: [9.085703252447725, 7.447003795211652]
+  },
+  {
+    name: "Gbenga Adeyemi & co Law Firm",
+    address: "Prince and Princess Abuja, district, Kaura, Abuja",
+    number: 8033513386,
+    website: "",
+    geocode: [9.001423821929109, 7.454803495210958]
+  },
+  {
+    name: "Streamsowers & Kohn",
+    address: "Stallion Estate, Flat CT 3, Off Lobito Cres, Wuse 2, Abuja",
+    number: "",
+    website: "http://www.sskohn.com",
+    geocode: [9.071440600014265, 7.491193708703368]
+  },
+  {
+    name: "Tcorporateconsult",
+    address: "Bright Star Plaza, 50 Ebitu Ukiwe St, Jabi, Abuja",
+    number: 8062348867,
+    website: "",
+    geocode: [9.066965365749645, 7.431353479867309]
+  },
+  {
+    name: "EVERLAW ASSOCIATES",
+    address: "Villa, No. 4, THE BROOK), [Ground Floor], Jemko, Aliade CL., Garki 2, Abuja",
+    number: 8124440006,
+    website: "",
+    geocode: [9.029525912813828, 7.494669652883304]
+  },
+  {
+    name: "Adekunle Olanipekun Legal Practitioners",
+    address: "14, Alexandria Crescent off Aminu Kano, Wuse 2, Abuja",
+    number: "",
+    website: "",
+    geocode: [9.084025359359304, 7.465740366375666]
+  },
+  {
+    name: "Pensbury Attorneys & Solicitors",
+    address: "14, Alexandria Crescent off Aminu Kano, Wuse 2, Abuja",
+    number: "",
+    website: "",
+    geocode: [9.084025359359304, 7.465740366375666]
+  }
 ]
