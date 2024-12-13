d9efd28147c654e76b240c730bda14fcf8ab0fbd
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-12-13 18:28:14 +0100

    Trying Adobe PDF Reader

--- public/js/adobereader.js
+++ public/js/adobereader.js
@@ -1,5 +1,5 @@
 document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-		var adobeDCView = new AdobeDC.View({clientId: "04bb7623260a4635abeb5d2105f6686e", divId: "adobe-dc-view"});
+		var adobeDCView = new AdobeDC.View({clientId: "50634c7ad18942388ecd45abe14999c2", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
 			content:{location: {url: "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf"}},
 			metaData:{fileName: "Bodea Brochure.pdf"}
