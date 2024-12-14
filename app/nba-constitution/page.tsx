e4eb97cde0afb785bdd87085ab4a9e7f9056a2ff
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-12-14 10:58:31 +0100

    Trying Adobe PDF Lightbox

--- app/nba-constitution/page.tsx
+++ app/nba-constitution/page.tsx
@@ -12,11 +12,16 @@ function NBAConstitution() {
                     <div className="xl:px-20 pt-28 px-5 md:px-10 min-h-[400px] flex items-center justify-center">
                         <div className='py-32 relative z-10'>
                             <h1 className='text-center'>Constitution of the NBA</h1>
-                            <p className='small-gold text-center'> Coming soon</p>
                         </div>
                     </div>
-                    <div className='flex justify-center relative z-10'>
-                        <div id="adobe-dc-view" className='w-auto max-w-[92%] h-screen xl:max-w-[1200px]'></div>
+                    <div className='flex flex-col items-center relative z-10 xl:px-20 px-5 md:px-10 min-h-[400px]'>
+                        <img src='nba-constitution.webp' className='object-contain cursor-pointer' id='LoadPDF'></img>
+                        <a className='button about-btn my-20' id='LoadPDF'>
+                            <div className='flex items-center btn hover:translate-x-2 gap-3.5'>
+                                <span className='text-center'>Read the constitution</span>
+                                <svg viewBox="0 0 32 32" width="30" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M22 9a1 1 0 0 0 0 1.42l4.6 4.6H3.06a1 1 0 1 0 0 2h23.52L22 21.59A1 1 0 0 0 22 23a1 1 0 0 0 1.41 0l6.36-6.36a.88.88 0 0 0 0-1.27L23.42 9A1 1 0 0 0 22 9Z" data-name="Layer 2" fill="#c9a66d" ></path></svg>
+                            </div>
+                        </a>
                     </div>
                 </section>
             </div>
--- public/js/adobereader.js
+++ public/js/adobereader.js
@@ -1,15 +1,32 @@
-document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-		var adobeDCView = new AdobeDC.View({clientId: "50634c7ad18942388ecd45abe14999c2", divId: "adobe-dc-view"});
-		adobeDCView.previewFile({
-			content:{location: {url: "NBAConstitution2021.pdf"}},
-			metaData:{fileName: "Bodea Brochure.pdf"}
-		}, {
-			embedMode: "IN_LINE",
-			enableLinearization: true,
-			// defaultViewMode: "FIT_PAGE",
-		});
-});
-	
-	// e9cedcbf5e5a4ba68d8d0190fee16c65
 
-	// www.nbaylfabuja.org: "04bb7623260a4635abeb5d2105f6686e"
+	
+// e9cedcbf5e5a4ba68d8d0190fee16c65
+
+// www.nbaylfabuja.org: "04bb7623260a4635abeb5d2105f6686e"
+	
+const elements = document.querySelectorAll('#LoadPDF');
+
+elements.forEach((element) => {
+    element.addEventListener('click', function() 
+	{
+		var adobeDCView = new AdobeDC.View(
+			{
+				clientId: "50634c7ad18942388ecd45abe14999c2"
+			}
+		);
+
+		adobeDCView.previewFile(
+			{
+				content: {
+					location: {url: "NBAConstitution2021.pdf"}
+				},
+				metaData: {
+					fileName: "NBA Constitution 2021.pdf"
+				}
+			},
+			{
+				embedMode: "LIGHT_BOX"
+			}
+		);
+	});
+});
